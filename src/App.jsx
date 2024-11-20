0b496442ab0b54a122cf04d533568e1de391e6bf
Krishnakumar046 <krishnakumar6046@gmail.com>
2024-11-20 12:34:19 +0530

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Navbar />
         <Routes>
           <Route path="/" element={<UseState />} />
